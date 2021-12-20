73fe1d4c4cb750c0942eb5067c7dbb17c54573bd
itay <itayt23@gmail.com>
2021-12-20 10:26:05 +0200

    Adding options to whatsapp to excel just numbers + fixing scrap stccuk in midlle writing

---
+++ MeGod.py
@@ -0,0 +1,65 @@
+import PySimpleGUI as sg
+from WhatsAppScrap import WhatsAppScrap
+from MeScrap import MeScrap
+from ExcelRead import ExcelReader 
+from window import Layout
+import pandas as pd
+
+
+layout = Layout()
+window = layout.setWindow(layout.getMainLayout())
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event=="Exit":
+        break
+    elif event == "Read from Excel file":
+        window.close()
+        window = sg.Window('Caller Finder',layout.getExcelLayout(), size=(750,350),element_justification='c')
+    elif event == "Submit" and values["-FILE_NAME-"] != "":
+        window["-PROG-"].UpdateBar(1)
+        file_path = values["-FILE-"]
+        excel_reader = ExcelReader()
+        clients_numbers = excel_reader.readFile(file_path)
+        window["-PROG-"].UpdateBar(2)
+        me_scraper = MeScrap(clients_numbers,values["-FILE_NAME-"])
+        window["-PROG-"].UpdateBar(3)
+        me_scraper.clients = me_scraper.ScrapClients(clients_numbers)
+        window["-PROG-"].UpdateBar(4)
+        me_scraper.writeFile()
+        window["-PROG-"].UpdateBar(5)
+        break
+    elif event == "Read from WhatsApp":
+        window.close()
+        window = sg.Window('Caller Finder',layout.getWhatsAppLayout(), size=(750,350),element_justification='c')
+    elif event == "Search" and values["-GROUP_NAME-"] != "":
+        window["-PROG-"].UpdateBar(1)
+        whatsapp_scraper = WhatsAppScrap(values["-GROUP_NAME-"])
+        window["-PROG-"].UpdateBar(2)
+        whatsapp_scraper.searchNumbers()
+        window["-PROG-"].UpdateBar(3)
+        whatsapp_scraper.fixNumbers()
+        window["-PROG-"].UpdateBar(4)
+        me_scraper = MeScrap(whatsapp_scraper.getNumbers(), values["-GROUP_NAME-"])
+        window["-PROG-"].UpdateBar(5)
+        me_scraper.ScrapClients()
+        window["-PROG-"].UpdateBar(6)
+        me_scraper.writeFile()
+        window["-PROG-"].UpdateBar(7)
+        break
+    elif event == "Read from WhatsApp - Numbers Only":
+        window.close()
+        window = sg.Window('Caller Finder',layout.getWhatsAppNumbersLayout(), size=(750,350),element_justification='c')
+    elif event == "Get Numbers" and values["-GROUP_NAME_2-"] != "":
+        window["-PROG-"].UpdateBar(1)
+        whatsapp_scraper = WhatsAppScrap(values["-GROUP_NAME_2-"])
+        window["-PROG-"].UpdateBar(2)
+        whatsapp_scraper.searchNumbers()
+        window["-PROG-"].UpdateBar(3)
+        whatsapp_scraper.fixNumbers()
+        window["-PROG-"].UpdateBar(4)
+        whatsapp_scraper.writeFile()
+        window["-PROG-"].UpdateBar(5)
+        break
+
+window.close()
---
+++ MeScrap.py
@@ -0,0 +1,68 @@
+from logging import ERROR
+from selenium.webdriver.chrome.webdriver import WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import selenium.webdriver as webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import sys
+import PySimpleGUI as sg
+
+
+class MeScrap:
+    numbers = []
+    clients = {}
+    group =""
+
+
+    def __init__(self,numbers,group):
+        self.numbers = numbers
+        self.group = group
+
+    def writeFile(self):
+        clients_df = pd.DataFrame(data=self.clients,index=[self.group])
+        clients_df = (clients_df.T)
+        group_name = self.group.replace('"',"").replace(':',"").replace('?',"").replace('\\',"").replace('/',"").replace('<',"").replace('>',"").replace('*',"").replace('|',"")
+        clients_df.to_excel(group_name+' לקוחות.xlsx')
+
+    def loadCookies(self):
+        dir_path = os.getcwd()
+        profile = os.path.join(dir_path, "profile", "wpp")
+        options = webdriver.ChromeOptions()
+        options.add_argument(r"user-data-dir={}".format(profile))
+        return options
+
+    def setBrowser(self):
+        try:
+            cookies = self.loadCookies()
+            url = "https://web.me.app/"
+            browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=cookies)
+            browser.get(url)
+            return browser
+        except Exception as e:
+            sg.popup_error(f"Oops!", e.__class__, "occurred.\n Error: Oppening browser or reading Url.")
+            sys.exit(1)
+
+    
+    def ScrapClients(self):
+        browser = self.setBrowser()
+        clients_dict = {}
+        search_box =  WebDriverWait(browser, 60).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[2]/div[2]/div/div[1]/input")))
+        try:
+            for number in self.numbers:
+                search_box.send_keys(number)
+                search_box_button = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[2]/div[2]/div/div[3]/div/button")))
+                search_box_button.click()
+                client_name = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[2]/div[3]/div/div[2]/div/div[2]/div[2]/div[1]/div[1]"))).text
+                clients_dict[client_name] = number
+                search_box.clear()
+            browser.close()
+            self.clients = clients_dict
+            #return clients_dict
+        except Exception as e:
+            self.clients = clients_dict
+            self.writeFile()
+            sg.popup_error(f"Oops!", e.__class__, "occurred.\n Error: Problem accured during the data reading!! the patch close before it was finish.")
+            sys.exit(1)
